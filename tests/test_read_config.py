a3314b728e29ea77d9e29e2c1bbf495ea4bfb30b
Shinya Sasaki <shinya.sasaki@kek.jp>
2018-09-12 15:30:34 +0900

    Clean codes

--- tests/test_read_config.py
+++ tests/test_read_config.py
@@ -6,11 +6,14 @@ from zetemple import reader
 def test_create_file(tmpdir):
     path = tmpdir.join("config.csv")
 
-    contents = "host1, prefix1:\n" \
-               "host2, prefix2:\n" \
-               "#comment,comment\n" \
-               "comment\n" \
-               "host3, prefix3:"
+    contents = (
+                "host1, prefix1:\n"
+                "host2, prefix2:\n"
+                "#comment,comment\n"
+                "comment\n"
+                "host3, prefix3:"
+               )
+
     path.write(contents)
 
     expected_hosts = [
